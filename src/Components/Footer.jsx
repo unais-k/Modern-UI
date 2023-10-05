12d0516b4fe7a921169e065cc2073ea751f7d377
unais-k <unaissafa008@gmail.com>
2023-10-05 22:05:11 +0530

    finish

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,13 +6,13 @@ import Accordion from "./Accordian";
 const Footer = () => (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
         <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
-            <div className="flex-1 flex flex-col justify-start mr-10">
+            <div className="flex-1 flex flex-col justify-center md:justify-start mr-10">
                 <img src={logo} alt="logo" className="w-[266px] h-[72.14px] object-contain" />
                 <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
                     A new way to make the payment easy, reliable and secure
                 </p>
             </div>
-            <div className="hidden sm:block w-full">
+            <div className="hidden sm:block">
                 <div className={`flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10`}>
                     {footerLinks.map((footerLink) => (
                         <div className="flex flex-col ss:my-0 my-4 min-w-[150px]" key={footerLink.id}>
@@ -37,18 +37,18 @@ const Footer = () => (
                     ))}
                 </div>
             </div>
-        </div>
-        <div className="sm:hidden block w-full m-10">
-            <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap">
-                {footerLinks.map((footerLink) => (
-                    <Accordion key={footerLink.id} {...footerLink} />
-                ))}
+            <div className="sm:hidden block w-full m-10">
+                <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap">
+                    {footerLinks.map((footerLink) => (
+                        <Accordion key={footerLink.id} {...footerLink} />
+                    ))}
+                </div>
             </div>
         </div>
 
         <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
             <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-                Copyright Ⓒ 2022 HooBank. All Rights Reserved.
+                Copyright Ⓒ 2023 HooBank. All Rights Reserved.
             </p>
 
             <div className="flex flex-row md:mt-0 mt-6">
